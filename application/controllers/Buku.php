d83ce3a6e71590cebdc885e368ed072c17c5502a
leobgs <leonardobagus093@gmail.com>
2022-07-08 21:20:15 +0700

    Membuat Ubah Kategori Buku

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -36,6 +36,35 @@ class Buku extends CI_Controller
         redirect('buku/kategori');
     }
 
+    public function updateKategori()
+    {
+        $data['judul'] = 'Ubah Data Kategori';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['kategori'] = $this->ModelBuku->kategoriWhere(['id_kategori' => $this->uri->segment(3)])->result_array();
+
+        $this->form_validation->set_rules('nama_kategori', 'Nama Kategori', 'required|min_length[3]', [
+            'required' => 'Nama Kategori harus diisi',
+            'min_length' => 'Nama Kategori terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('admin/header', $data);
+            $this->load->view('admin/sidebar', $data);
+            $this->load->view('admin/topbar', $data);
+            $this->load->view('buku/ubah_kategori', $data);
+            $this->load->view('admin/footer');
+        } else {
+
+            $data = [
+                'nama_kategori' => $this->input->post('nama_kategori', true)
+            ];
+
+            $this->ModelBuku->updateKategori(['id_kategori' => $this->input->post('id_kategori')], $data);
+            redirect('buku/kategori');
+        }
+    }
+
+
     public function hapusBuku()
     {
         $where = ['id' => $this->uri->segment(3)];
